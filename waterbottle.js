239d8e118e27dda4c5e735bc80bbaff28b6203d3
CharulathA1432 <125582832+CharulathA1432@users.noreply.github.com>
2023-08-15 21:05:12 +0530

    Add files via upload

---
+++ waterbottle.js
@@ -0,0 +1,44 @@
+status = "";
+water_bottle_img = "";
+objects = [];
+
+function preload(){
+    water_bottle_image = loadImage("water_bottle.png");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_detector = ml5.object_detector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_detector.detect(water_bottle_image,gotResults);
+}
+
+function gotResults(results,error){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(water_bottle_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("Status").innerHTML = "Status : Objects Detected";
+        
+            fill("#fc0303");
+            percent = floor(objects(i).confidence * 100);
+            text(objects(i).label + " " + percent + "%",objects(i).height);
+            noFill();
+            stroke("#fc0303");
+            Rect(objects(i).x, objects(i).y, objects(i).width, objects(i).height);
+        }
+    }
+}
